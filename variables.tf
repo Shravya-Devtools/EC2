469ae7900d9ed388471bb12b5737d3417b7cc279
Shravya-Devtools <shravyam@devtools.in>
2025-08-06 09:55:04 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0c1a7f89451184c8b" # Amazon Linux 2 (Mumbai)
+}
